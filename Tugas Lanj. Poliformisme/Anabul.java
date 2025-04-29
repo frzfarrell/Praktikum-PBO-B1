71429aa1476de543d453bf6a29b2bd472784aea4
frzfarrell <frzfarrell11@gmail.com>
2025-04-29 16:04:09 +0700

    Add files via upload

---
+++ Tugas Lanj. Poliformisme/Anabul.java
@@ -0,0 +1,8 @@
+package SMT4.PBO.Tugas Lanj. Poliformisme;
+
+public abstract class Anabul {
+
+    public abstract void Gerak();
+
+    public abstract void Bersuara();
+}
---
+++ Tugas Lanj. Poliformisme/Kucing.java
@@ -0,0 +1,14 @@
+package SMT4.PBO.Tugas Lanj. Poliformisme;
+
+public class Kucing extends Anabul {
+    @Override
+    public void Gerak() {
+        System.out.println("Kucing bergerak dengan melata");
+    }
+    
+    @Override
+    public void Bersuara() {
+        System.out.println("Kucing bersuara: Meong!");
+    }
+}
+
