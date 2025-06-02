3b6c510c20f06f638c2e87d1a836fcca0d908c11
frzfarrell <frzfarrell11@gmail.com>
2025-06-02 14:40:49 +0700

    Add files via upload

---
+++ Praktikum 7/src/Test_medium/KontrolSenjata.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test_medium;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KontrolSenjata {
+    
+}
